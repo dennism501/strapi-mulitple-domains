6acf7254d6d1edfba17ab256c7a266d471aea313
Dennis Mubamba <mubambadennis@gmail.com>
2022-04-11 11:26:04 +0200

    Added Github SSO

--- config/admin.js
+++ config/admin.js
@@ -1,5 +1,31 @@
+const GithubStrategy = require("passport-github2");
+
 module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET', 'f9f6bf0734e8a0b3d2910f9170ab8eb0'),
+
+    providers: [
+      {
+        uid: "github",
+        displayName: "Github",
+        icon: "https://cdn1.iconfinder.com/data/icons/logotypes/32/github-512.png",
+        createStrategy: (strapi) =>
+          new GithubStrategy(
+            {
+              clientID: env("GITHUB_CLIENT_ID"),
+              clientSecret: env("GITHUB_CLIENT_SECRET"),
+              scope: ["user:email"],
+              callbackURL:
+                strapi.admin.services.passport.getStrategyCallbackURL("github"),
+            },
+            (accessToken, refreshToken, profile, done) => {
+              done(null, {
+                email: profile.emails[0].value,
+                username: profile.username,
+              });
+            }
+          ),
+      },
+    ],
   },
 });
