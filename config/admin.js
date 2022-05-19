188e7a928f79c63fc9a46a1c01705b98628142fc
Dennis Mubamba <mubambadennis@gmail.com>
2022-05-19 12:55:04 +0200

    Added articles and domains

--- config/admin.js
+++ config/admin.js
@@ -3,7 +3,6 @@ const GithubStrategy = require("passport-github2");
 module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET', 'f9f6bf0734e8a0b3d2910f9170ab8eb0'),
-
     providers: [
       {
         uid: "github",
